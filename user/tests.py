cf79a33c522fa7cc225d0b169bd6cb04a3f9b24d
supersaloon <supersaloon@gmail.com>
2021-01-05 15:51:36 +0900

    [Add] feature/user-signup: 회원가입 기능 및 테스트 구현 - 회원가입 기능 및 테스트 구현   - 회원가입 성공한 경우 201 리턴   - 형식에 맞지 않는 패스워드를 입력한 경우 400 리턴   - 형식에 맞지 않는 이메일을 입력한 경우 400 리턴   - email로 이미 가입된 경우 400 리턴   - email(카카오 계정)으로 이미 가입된 경우 400 리턴   - 문자 메세지 인증이 되어 있지 않은 경우 400 리턴
      - PR#의 리뷰 반영하여 수정
        - 여러 메소드에서 사용될 변수는 setUp에 선언하고 불러오도록 수정
        - utils.py 의 빈칸 삭제
        - `account_type_id = 2` 와 같이 사용하던 것을 -> `email_account_type = AccountType.objects.get(name="email")` 로 수정

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,122 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test      import Client
+from django.test      import TestCase
+
+from user.models import Authentication, User, AccountType
+
+class SignUpTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        AccountType(name='email').save() # 1
+        AccountType(name='kakao').save() # 2
+
+        Authentication(
+            phone_number     = '01012345678',
+            sms_number       = '123456',
+            is_authenticated = True,
+        ).save()
+
+
+    def setUp(self):
+        self.new_user = {
+            'email'       : 'homer@homer.com',
+            'password'    : '12345qwert!@#$%',
+            'nickname'    : 'Homer Jay Simpson',
+            'phone_number': '01012345678',
+            'sms_number'  : '123456',
+        }
+
+        self.client = Client()
+
+        self.email_account_type = AccountType.objects.get(name="email")
+        self.kakao_account_type = AccountType.objects.get(name="kakao")
+
+
+
+    def test_signup_success(self):
+        response = self.client.post('/user/signup', json.dumps(self.new_user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_signup_fail_with_poor_password(self):
+        poor_password_user = {
+            'email'       : 'homer@homer.com',
+            'password'    : 'password',
+            'nickname'    : 'Homer Jay Simpson',
+            'phone_number': '01012345678',
+            'sms_number'  : '123456',
+        }
+        response = self.client.post('/user/signup', json.dumps(poor_password_user), content_type='application/json')
+
+        self.assertEqual(response.json(), {"MESSAGE": "INVALID PASSWORD"})
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_signup_fail_with_poor_email(self):
+        poor_email_user = {
+            'email'       : 'homer#homer.com',
+            'password'    : '12345qwert!@#$%',
+            'nickname'    : 'Homer Jay Simpson',
+            'phone_number': '01012345678',
+            'sms_number'  : '123456',
+        }
+
+        response = self.client.post('/user/signup', json.dumps(poor_email_user), content_type='application/json')
+
+        self.assertEqual(response.json(), {"MESSAGE": "INVALID EMAIL"})
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_signup_fail_with_authentication_fail(self):
+        authentication = Authentication.objects.get(phone_number = '01012345678')
+        authentication.is_authenticated = False
+        authentication.save()
+
+        response = self.client.post('/user/signup', json.dumps(self.new_user), content_type='application/json')
+
+        self.assertEqual(response.json(), {"MESSAGE": "AUTHENTICATION FAIL"})
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_signup_fail_with_email_duplication(self):
+        Authentication(
+            phone_number     = '01011112222',
+            sms_number       = '111222',
+            is_authenticated = True,
+        ).save()
+
+        User(
+            email        = 'homer@homer.com',
+            nickname     = 'King God Homer',
+            password     = '12345qwert!@#$%',
+            phone_number = '01011112222',
+            account_type = self.email_account_type,
+        ).save()
+
+        response = self.client.post('/user/signup', json.dumps(self.new_user), content_type='application/json')
+
+        self.assertEqual(response.json(), {"MESSAGE": "EMAIL ACCOUNT EXISTS"})
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_signup_fail_with_already_kakao_signed_up_with_email(self):
+        Authentication(
+            phone_number     = '01011112222',
+            sms_number       = '111222',
+            is_authenticated = True,
+        ).save()
+
+        User(
+            email        = 'homer@homer.com',
+            nickname     = 'King God Homer',
+            password     = '12345qwert!@#$%',
+            phone_number = '01011112222',
+            account_type = self.kakao_account_type,
+        ).save()
+
+        response = self.client.post('/user/signup', json.dumps(self.new_user), content_type='application/json')
+
+        self.assertEqual(response.json(), {"MESSAGE": "KAKAO ACCOUNT EXISTS"})
+        self.assertEqual(response.status_code, 400)
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from .views.signup_views import SigupUpView
+
+
 urlpatterns = [
+    path('/signup', SigupUpView.as_view()),
 ]
