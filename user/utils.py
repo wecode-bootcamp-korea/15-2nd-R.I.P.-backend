533c1fa91237a3428285cff47b4815174f7822f6
groundzerodayattack <groundzerodayattack@gmail.com>
2021-01-07 22:19:36 +0900

    [Add] feature/user-signin: 로그인 기능 및 테스트 구현 - 로그인 기능 및 테스트 구현   - 회원가입된 email 및 password와 일치하는 값을 전달받으면 로그인 처리     - status=200 및 ACCESS_TOKEN 리턴   - 존재하지 않는 email로 로그인 시도하면 400 리턴   - email은 존재하나 password가 일치하지 않으면 400 리턴
      - PR#5 리뷰 반영하여 수정
        - 이전 PR에 해당되는 파일 삭제
        - 반복되는 코드는 setUp에 담아서 사용
        - signup 관련 코드 삭제
        - utils.py 공백 삭제
    
      - 로그인 데코레이터 추가
    
    [Modify] 데코레이터 구현 후 conflict 해결

--- user/utils.py
+++ user/utils.py
@@ -1,2 +1,32 @@
+import jwt
+from functools import wraps
+
+from django.http import JsonResponse
+
+from my_settings import SECRET_KEY, ENCRYPTION_ALGORITHM
+from user.models import User
+
+
 REGEX_PASSWORD = '^(?=.*[0-9])(?=.*[!@#$%^&*()_+])(?=.*[a-zA-Z]).{10,}$'  # 영문+숫자+특수문자 10자 이상
 REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+
+def login_required(func):
+    @wraps(func)
+    def decorated_function(self, request, *args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token is not None:
+            try:
+                decoded_access_token = jwt.decode(access_token, SECRET_KEY, algorithms=ENCRYPTION_ALGORITHM)
+                email                = decoded_access_token['id']
+                user                 = User.objects.get(email=email)
+                request.user         = user
+            except jwt.InvalidTokenError:
+                return JsonResponse({'MESSAGE': 'INVALID ACCESS TOKEN'}, status=401)
+            except User.DoesNotExist:
+                return JsonResponse({'MESSAGE': 'USER NOT EXIST'}, status=401)
+        else:
+            return JsonResponse({'MESSAGE': 'ACCESS TOKEN NOT EXIST'}, status=401)
+
+        return func(self, request, *args, **kwargs)
+    return decorated_function
